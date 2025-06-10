a6b5695bdbeaad8fe0a5a355b6a038755db95095
Raul Albuquerque <raulalbuquerque2014@gmail.com>
2025-06-10 15:05:18 -0300

    build: create backend dev dependences

---
+++ backend/app/main.py
@@ -0,0 +1,15 @@
+from typing import Union
+
+from fastapi import FastAPI, HTTPException, Depends
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "Raul"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
