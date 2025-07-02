a65be0b2094f483f39fec35dcc75836d03e979bb
Raul Albuquerque <raulalbuquerque2014@gmail.com>
2025-07-02 13:05:41 -0300

    chore: create cors config

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,10 +1,19 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.database import Base, engine
 from app.models import url_model, access_model
 from .api.v1 import api_v1_router
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["GET", "POST"],
+    allow_headers=["*"],
+)
 
 Base.metadata.create_all(bind=engine)
 
