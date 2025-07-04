f748776d7a88778f519515b496db6c7c9e350c52
Raul Albuquerque <raulmalbuquerque2014@gmail.com>
2025-07-04 12:42:51 -0300

    feat: improve middleware private routes protection

--- frontend/middleware.ts
+++ frontend/middleware.ts
@@ -1,17 +1,28 @@
+import { jwtVerify } from "jose"
 import { NextRequest, NextResponse } from "next/server"
+import { deleteTokenOnCookies } from "./actions/cookies"
 
+
+const SECRET = new TextEncoder().encode(process.env.NEXT_PUBLIC_AUTH_SECRET_KEY!)
 export const TOKEN_KEY = "token"
 
-export function middleware(request: NextRequest) {
+export async function middleware(request: NextRequest) {
   const token = request.cookies.get(TOKEN_KEY)
   const protectedRoutes = ["/dashboard"]
   const isProtectedRoute = protectedRoutes.includes(request.nextUrl.pathname)
 
-  if (isProtectedRoute && !token) {
+  if (isProtectedRoute && !token  || !token?.value) {
     return NextResponse.redirect(new URL("/", request.url))
   }
 
-  return NextResponse.next()
+  try {
+    await jwtVerify(token.value, SECRET)
+    return NextResponse.next()
+
+  } catch (err) {
+    console.error("Token inválido:", err)
+    deleteTokenOnCookies()
+  }
 }
 
 export const config = {
